e52d5498d66c440f8fb8efc81d452a8c0c76571c
BeCoded126 <77804389+BeCoded126@users.noreply.github.com>
2025-02-05 04:50:29 +0000

    adding css

--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
-import "bootstrap";
-import "./style.css";
+let sentence = document.getElementById("excuse");
+let button = document.getElementById("button");
 
+let who = ["My Cat", "My Kid", "My Grandma", "My Mom"];
+let what = ["Slept", "Slid", "Tripped", "Twirled"];
+let item = ["On the Floor", "On the Road", "On the Ice", "On the Carpet"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
+button.addEventListener("click", function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  let randomNumberFirst = Math.floor(Math.random() * who.length);
+  let randomNumberSecond = Math.floor(Math.random() * what.length);
+  let randomNumberThird = Math.floor(Math.random() * item.length);
+
+  let first = who[randomNumberFirst];
+  let second = what[randomNumberSecond];
+  let third = item[randomNumberThird];
+
+  return (sentence.innerHTML = first + " " + second + " " + third + ".");
+});
